bde7af99f6a5d02ac13f0d24375bbe20201a04d1
Vyacheslav Kuzmin <vyvkuzmin@greenatom.ru>
2022-12-17 08:24:30 +0300

    .

--- miscell/web/send_files.py
+++ miscell/web/send_files.py
@@ -6,16 +6,22 @@ import aiohttp
 
 
 async def send_files(endpoint: str, files: list):
-    for file in files:
-        async with aiohttp.ClientSession() as session:
-            f = open(file, "rb")
-            async with session.put(endpoint, data=f, ssl=False) as resp:
-                print(await resp.text())
-            f.close()
+    async with aiohttp.ClientSession() as session:
+        # data = aiohttp.FormData()
+        # for file in files:
+        #     data.add_field('file', open(file, "rb"), filename=file.name)
+        with aiohttp.MultipartWriter('mixed') as mpwriter:
+            for file in files:
+                with aiohttp.MultipartWriter('related') as subwriter:
+                    # subwriter.append_payload(aiohttp.Payload(open(file, "rb")))
+                    ...
+            mpwriter.append_(subwriter)
+        async with session.put(endpoint, data=mpwriter) as resp:
+            print(await resp.text())
 
 if __name__ == "__main__":
     files = list(Path(".").glob("files/*"))
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(send_files("http://localhost:8080/upload", files))
+    loop.run_until_complete(send_files("http://localhost:8080/upload/", files))
     loop.close()
