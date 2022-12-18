8e0bb99c84febb85fb0ccb02de2ea292ceb9fd29
Vyacheslav Kuzmin <vyacheslav.v.kuzmin@gmail.com>
2022-12-18 21:59:14 +0300

    Отправка файлов в multipart/form-data

--- miscell/web/send_files.py
+++ miscell/web/send_files.py
@@ -1,27 +1,31 @@
 import asyncio
+import logging
 
 from pathlib import Path
 
 import aiohttp
 
+async def on_request_start(session, trace_config_ctx, params):
+    print("Starting request")
+
 
 async def send_files(endpoint: str, files: list):
-    async with aiohttp.ClientSession() as session:
-        # data = aiohttp.FormData()
-        # for file in files:
-        #     data.add_field('file', open(file, "rb"), filename=file.name)
-        with aiohttp.MultipartWriter('mixed') as mpwriter:
-            for file in files:
-                with aiohttp.MultipartWriter('related') as subwriter:
-                    # subwriter.append_payload(aiohttp.Payload(open(file, "rb")))
-                    ...
-            mpwriter.append_(subwriter)
-        async with session.put(endpoint, data=mpwriter) as resp:
+    trace_config = aiohttp.TraceConfig()
+    trace_config.on_request_start.append(on_request_start)
+    async with aiohttp.ClientSession(trace_configs=[trace_config]) as session:
+        data = aiohttp.FormData()
+        for index, file in enumerate(files):
+            if len(files) == 1:
+                index = ""
+            data.add_field(f'file{index}', open(file, "rb"), filename=file.name)
+            data.add_field(name='body', value=f'This is file {file.name}')
+
+        async with session.post(endpoint, data=data) as resp:
             print(await resp.text())
 
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     files = list(Path(".").glob("files/*"))
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(send_files("http://localhost:8080/upload/", files))
-    loop.close()
+    asyncio.run(send_files("http://localhost:9980/upload", files))
